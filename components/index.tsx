66e965d89698ff4b92e300f32271b7137ea0bd6c
Kaushik Tripathi <kaushiktripathi847@gmail.com>
2024-08-11 19:31:41 +0530

    initial commit

---
+++ components/index.tsx
@@ -0,0 +1,27 @@
+import About from "./About";
+import BackgroundCircles from "./BackgroundCircles";
+import Contact from "./Contact";
+import DarkModeToggle from "./DarkModeToggle";
+import Experience from "./Experience";
+import ExperienceCard from "./ExperienceCard";
+import Header from "./Header";
+import Hero from "./Hero";
+import Projects from "./Projects";
+import Skills from "./Skills";
+import Skillscrolling from "./SkillsScrolling";
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+
+export {
+  About,
+  BackgroundCircles,
+  Contact,
+  DarkModeToggle,
+  Experience,
+  ExperienceCard,
+  Header,
+  Hero,
+  Projects,
+  Skills,
+  Skillscrolling,
+  InfiniteMovingCards,
+};
